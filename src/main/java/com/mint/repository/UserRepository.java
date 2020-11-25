42c57981ecc05e2e5c9e88df4e99e681b6cae33d
intellisr <intellisr@gmail.com>
2020-11-25 23:05:18 +0530

    drool 1

--- src/main/java/com/mint/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mint.repository;
-
-import com.mint.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// This interface is extending Java Persistent API which maps the 'User' model
-public interface UserRepository extends JpaRepository<User,Integer> {
-
-}
