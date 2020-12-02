fa03c118403d264261648ec4f09ad440ae44b0cd
intellisr <intellisr@gmail.com>
2020-12-02 12:03:17 +0530

    Kie server integrated

--- src/main/java/com/mint/Services/Impl/UserImpl.java
+++ src/main/java/com/mint/Services/Impl/UserImpl.java
@@ -14,10 +14,8 @@ import org.kie.internal.command.CommandFactory;
 import org.kie.server.api.marshalling.MarshallingFormat;
 import org.kie.server.api.model.KieServiceResponse;
 import org.kie.server.api.model.ServiceResponse;
-import org.kie.server.client.KieServicesClient;
-import org.kie.server.client.KieServicesConfiguration;
-import org.kie.server.client.KieServicesFactory;
-import org.kie.server.client.RuleServicesClient;
+import org.kie.server.client.*;
+import org.kie.server.client.credentials.EnteredCredentialsProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -27,29 +25,22 @@ import java.util.List;
 
 @Service //This tells that we will be using this as a Service
 public class UserImpl implements UserInterface {
+
+    public User userUpdated;
+
     @Autowired //This means below is a dependency
     private UserRepository userRepository;
 
     ////////////////////////LOCAL///////////////////////////////////////////////////////////
-    KieServices kieServices = KieServices.Factory.get();
-    KieContainer kContainer = kieServices.getKieClasspathContainer();
+    // KieServices kieServices = KieServices.Factory.get();
+    // KieContainer kContainer = kieServices.getKieClasspathContainer();
 
     ////////////////////////KIE SERVER///////////////////////////////////////////////////////////
     private static final String URL = "http://localhost:8180/kie-server/services/rest/server";
     private static final String USER = "admin";
     private static final String PASSWORD = "admin";
-
     private static final MarshallingFormat FORMAT = MarshallingFormat.JSON;
-
-    private static KieServicesConfiguration conf;
-
-    // KIE client for common operations
-    private static KieServicesClient kieServicesClient;
-
-    // Rules client
-    private static RuleServicesClient ruleClient;
-
-    private static final String CONTAINERID = "RestTest";
+    private static final String CONTAINERID = "com.mint:model:1.0.0-SNAPSHOT";
     private static final String CLASS_NAME = "User";
 
 
@@ -58,47 +49,57 @@ public class UserImpl implements UserInterface {
         return userRepository.findAll();
     }
 
+
     @Override
-    public String saveUser(User user){
+    public String saveUser(User user) {
         userRepository.save(user);
         return "success";
     }
 
-    @Override
-    public String getEmail(int userID){
 
-        User user=userRepository.findById(userID).get();
+    /// Kie Server Integration.
+    @Override
+    public String getEmail(int userID) {
+
+        User user = userRepository.findById(userID).get();
+
+
 
         ////////////////////////LOCAL///////////////////////////////////////////////////////////
 
-        KieSession kieSession = kContainer.newKieSession("rulesSession");
-        kieSession.insert(user);
-        kieSession.fireAllRules();
-        kieSession.dispose();
+//       KieSession kieSession = kContainer.newKieSession("rulesSession");
+//       kieSession.insert(user);
+//       kieSession.fireAllRules();
+//       kieSession.dispose();
 
-        ////////////////////////KIE SERVER///////////////////////////////////////////////////////////
 
-//        conf = KieServicesFactory.newRestConfiguration(URL, USER, PASSWORD);
-//        conf.setMarshallingFormat(FORMAT);
-//        kieServicesClient = KieServicesFactory.newKieServicesClient(conf);
-//
-//        RuleServicesClient rulesClient = kieServicesClient.getServicesClient(RuleServicesClient.class);
-//        KieCommands commandsFactory = KieServices.Factory.get().getCommands();
-//
-//        Command<?> insert = commandsFactory.newInsert(user,CLASS_NAME);
-//        Command<?> fireAllRules = commandsFactory.newFireAllRules();
-//        Command<?> batchCommand = commandsFactory.newBatchExecution(Arrays.asList(insert, fireAllRules));
-//
-//        ServiceResponse executeResponse = rulesClient.executeCommands(CONTAINERID,batchCommand);
-//
-//        if(executeResponse.getType() == KieServiceResponse.ResponseType.SUCCESS) {
-//            System.out.println("Commands executed with success! Response: ");
-//            System.out.println(executeResponse.getResult());
-//        } else {
-//            System.out.println("Error executing rules. Message: ");
-//            System.out.println(executeResponse.getMsg());
-//        }
 
-        return user.getEmail();
+
+
+        ////////////////////////KIE SERVER////////////////////////////////////////////////////////
+
+        KieServicesConfiguration config = KieServicesFactory.newRestConfiguration(URL, USER, PASSWORD, 120000);
+        config.setMarshallingFormat(FORMAT);
+
+        RuleServicesClient client = KieServicesFactory.newKieServicesClient(config).getServicesClient(RuleServicesClient.class);
+        List<Command<?>> cmds = new ArrayList<Command<?>>();
+        KieCommands commands = KieServices.Factory.get().getCommands();
+        cmds.add(commands.newInsert(user, CLASS_NAME));
+        cmds.add(commands.newFireAllRules());
+        BatchExecutionCommand myCommands = CommandFactory.newBatchExecution(cmds);
+        ServiceResponse<ExecutionResults> response = client.executeCommandsWithResults(CONTAINERID, myCommands);
+
+        if (response.getType() == KieServiceResponse.ResponseType.SUCCESS) {
+            System.out.println("Success");
+            userUpdated = (User) response.getResult().getValue(CLASS_NAME);
+
+        } else {
+            System.out.println("Error");
+            System.out.println(response.getMsg());
+        }
+
+        return userUpdated.getEmail();
     }
+
+
 }
